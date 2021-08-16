340076bd56bf2df120fe73ce0240c8b0defee974
Emanoel Rair Amâncio Lima <rair88@gmail.com>
2021-08-16 00:54:06 -0300

    APS-Atividade 1.1 - primeira implementação.

---
+++ Aluno.java
@@ -0,0 +1,42 @@
+public class Aluno {
+
+    // atributos
+    String nome;
+    String matricula;
+    Curso curso;
+
+    // construtores
+    public Aluno() {
+    }
+
+    public Aluno(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Aluno(String nome, String matricula, Curso curso) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    // gets sets e metodos
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return this.matricula;
+    }
+
+    public Curso getCurso() {
+        return this.curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+}
---
+++ Curso.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+public Class Curso{
+    //atributos
+    String nome;
+    String desc;
+    int codigo;
+    HashMap<Integer,String> matriculados =new HashMap<Integer,String>();
+    
+    //construtores
+    public Curso(){}
+    
+    public Curso(int codigo,String nome){
+        this.codigo=codigo;
+        this.nome=nome;
+    }
+    
+    public Curso(int codigo,String nome,String desc){
+        this.codigo=codigo;
+        this.nome=nome;
+        this.desc=desc;
+    }
+
+    //get sets e metodos
+
+    public void matricularAluno(Aluno aluno){
+        this.matriculados.put(aluno.getMatricula(),aluno);
+    }
+
+    public int getCodigo(){
+        return this.codigo;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public vois setNome(String nome){
+        this.nome=nome;
+    }
+
+    public String getDescricao(){
+        return this.desc;
+    }
+
+    public void setDescricao(String desc){
+        this.desc=desc;
+    }
+
+    public Aluno getAluno(String matricula){
+        return matriculados.get(matricula);
+    }
+}
